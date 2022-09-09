0e58f2fddc214fdf08160c445f76e1497e1869d8
Eric G. Navarro <ericgnavarro99@gmail.com>
2022-09-09 13:47:51 +0200

    New changes.
    - Camera path system.

--- ScubaGames/Assets/Scripts/CameraPath.cs
+++ ScubaGames/Assets/Scripts/CameraPath.cs
@@ -7,37 +7,43 @@ using UnityEngine;
 public class FollowPath
 {
     public bool _active = false;
-
-    [Space, Header("Gizmos Options:")]
+    [Space] public float _sphereGizmosRadius = .1f;
+    [Space] public Vector3 _sphereGizmosPosition;
+    [Space] public List<int> _gizmosTargets;
+    [Space] public List<Transform> _referenceBlocks;
+}
+public class CameraPath : MonoBehaviour
+{
+    [Space, Header("Gizmos options:")]
     public Color _sphereGizmosColor = Color.black;
     public float _sphereGizmosRadius = .1f;
     public Color _linearGizmosColor = Color.black;
 
-    [Space] public Vector3 _spherePosition = Vector3.zero;
-
-    [Space] public List<Transform> _targets;
-
-    [Space] public List<Transform> _referenceBlock = new List<Transform>();
-}
-public class CameraPath : MonoBehaviour
-{
     [Space] public List<FollowPath> _cameraPaths = new List<FollowPath>();
-
+    
     private void OnDrawGizmos()
     {
         if (_cameraPaths == null) return;
 
         for (int a = 0; a < _cameraPaths.Count; a++)
         {
-            if (_cameraPaths[a]._referenceBlock == null) return;
+            if (!_cameraPaths[a]._active) continue;
 
-            Gizmos.color = _cameraPaths[a]._active ? _cameraPaths[a]._linearGizmosColor : Color.clear;
+            Gizmos.color = _sphereGizmosColor;
+            Gizmos.DrawSphere(_cameraPaths[a]._sphereGizmosPosition, _cameraPaths[a]._sphereGizmosRadius);
 
-            /* Encontrar manera de acceder a todas las posiciones de target de cada list.
-            for (int b = 0; b < _cameraPaths[a]._targets.Count; b++)
-                Gizmos.DrawLine(_cameraPaths[a]._spherePosition, _cameraPaths[a]._targets.ToArray().GetValue(b));
-            */
-            
+            try
+            {
+                for (int b = 0; b < _cameraPaths[a]._gizmosTargets.Count; b++)
+                {
+                    Gizmos.color = _cameraPaths[a]._active ? _linearGizmosColor : Color.clear;
+                    Gizmos.DrawLine(_cameraPaths[a]._sphereGizmosPosition, _cameraPaths[_cameraPaths[a]._gizmosTargets[b]]._sphereGizmosPosition);
+                }
+            }
+            catch (System.ArgumentOutOfRangeException)
+            { 
+                Debug.LogError("Error: CameraPaths > CameraPath script > Targets in out of range.");
+            }
         }
     }
 }
